bd9e9cbf628ddd50dad31c37f5b27e3fdd415766
Nora Peters <norapeters5@gmail.com>
2017-08-28 21:19:04 -0400

    test conditional statements, cases

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ let number = 0;
 //   number = number + 2;
 // }
 
-for (let i = 0; i <= 12; i += 2) {
-  console.log(i);
+// for (let i = 0; i <= 12; i += 2) {
+//   console.log(i);
+// }
+
+// if (1 > 2) {
+//   console.log("One is less than 2");
+// } else if (2 > 1) {
+//   console.log("Bananas are green");
+// }
+
+let debt = 50;
+
+switch (debt) {
+  case 50:
+    console.log("You need to pay some money");
+    break;
+  case 40:
+    console.log("You should pay some money");
+    break;
 }
